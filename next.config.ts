3f316f47c34d01d6ee587cc440954e2981005567
yongarframe <yongar2002@gmail.com>
2025-06-11 13:43:13 +0900

    🌱 맛집 렌더링 완료
    맛집 렌더링 완료

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3001",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/types/placestype.ts
@@ -0,0 +1,11 @@
+export type Place = {
+  id: string;
+  title: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  lat: number;
+  lon: number;
+  description: string;
+}[];
